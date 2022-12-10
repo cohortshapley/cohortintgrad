2ae48aea282119c26bbb6050a6ca4d6afef166b6
Naofumi Hama <naofumi.hama.hd@hitachi.com>
2022-12-10 23:33:10 +0900

    add a test where IGCS and CS dont match

--- tests/test_igcs_toy.py
+++ tests/test_igcs_toy.py
@@ -2,6 +2,7 @@ import pytest
 import torch
 import math
 import cohortintgrad as csig
+import numpy as np
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 @pytest.fixture()
@@ -16,4 +17,11 @@ def ref_t():
 
 def test_igcs_single(ref, ref_t):
     IG = csig.CohortIntGrad(ref.to(device), ref_t.to(device), ratio=0.1, n_step=500)
-    assert math.isclose(-0.5, IG.igcs_single(0)[0], rel_tol=0.01)
+    igcs_single0 = IG.igcs_single(0) #(-0.5, -0.5) in analytical result but contains numerical error in practice
+    assert math.isclose(-0.5, igcs_single0[0], rel_tol=0.01)
+
+def test_igcs_single_2(ref, ref_t): #the case where IGCS and CS dont match
+    IG = csig.CohortIntGrad(ref.to(device), ref_t.to(device), ratio=0.1, n_step=500)
+    igcs_single3 = IG.igcs_single(3)
+    analytic = 3/7 + 2/(7 * np.sqrt(7)) * (np.arctan(1/np.sqrt(7)) - np.arctan(5/np.sqrt(7))) # analytic result
+    assert math.isclose(analytic, igcs_single3[0], rel_tol=0.01)
