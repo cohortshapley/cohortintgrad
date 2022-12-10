fd0f278ce24f3d85a2a23ddb99e2c73276e25f24
Naofumi Hama <naofumi.hama.hd@hitachi.com>
2022-12-11 00:03:14 +0900

    add tests of similarity function in the toy ex

--- tests/test_igcs_toy.py
+++ tests/test_igcs_toy.py
@@ -25,3 +25,20 @@ def test_igcs_single_2(ref, ref_t): #the case where IGCS and CS dont match
     igcs_single3 = IG.igcs_single(3)
     analytic = 3/7 + 2/(7 * np.sqrt(7)) * (np.arctan(1/np.sqrt(7)) - np.arctan(5/np.sqrt(7))) # analytic result
     assert math.isclose(analytic, igcs_single3[0], rel_tol=0.01)
+
+def test_summation_rule(ref, ref_t):
+    IG = csig.CohortIntGrad(ref.to(device), ref_t.to(device), ratio=0.1, n_step=500)
+    igcs_single3 = IG.igcs_single(3)
+    summation = torch.sum(igcs_single3).item()
+    y_diff = (ref_t[3] - torch.mean(ref_t)).item()
+    assert math.isclose(y_diff, summation, rel_tol=0.01)
+
+def test_dissimilarity(ref, ref_t):
+    IG = csig.CohortIntGrad(ref.to(device), ref_t.to(device), ratio=0.1, n_step=500)
+    dissimilar = IG._similarity(ref[:,1], ref[3,1]).to('cpu').detach().numpy() # "2" in ref is not similar to other 2nd element of ref but pick itself
+    np.testing.assert_array_equal(dissimilar, np.array([0,0,0,1]).astype(np.int32))
+
+def test_similar_ratio(ref, ref_t):
+    IG = csig.CohortIntGrad(ref.to(device), ref_t.to(device), ratio=0.5, n_step=500) # similar ratio is so large that similar to "1" in 2nd data
+    dissimilar = IG._similarity(ref[:,1], ref[3,1]).to('cpu').detach().numpy()
+    np.testing.assert_array_equal(dissimilar, np.array([0,1,0,1]).astype(np.int32))
